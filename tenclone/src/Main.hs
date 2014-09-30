1808affee8e5315378c77fa6134de79c1f96bb4a
raymoo <notanemail@fakeaddress.net>
2014-09-29 20:37:01 -0700

    Dummy response for new account requests

--- tenclone/src/Main.hs
+++ tenclone/src/Main.hs
@@ -13,10 +13,18 @@ site :: Snap ()
 site =
     ifTop (writeBS "Basic server that doesn't do anything interesting.") <|>
     route [ ("api/last_track_record", writeBS "2010-09-27T22:52:00+00:00")
+          , ("api/account", newAccountHandler)
           , ("game/:id/account/:username", accountHandler)
           ] <|>
     writeBS "There is nothing here." <|>
     dir "static" (serveDirectory ".") -- This will never handle anything as the code is currently written
+
+newAccountHandler :: Snap ()
+newAccountHandler = 
+    writeBS "Your request body: \n" >>
+    readRequestBody maxBound >>= writeLBS >>
+    writeBS "\nThis is a test. No accounts were created."
+                    
 
 accountHandler :: Snap ()
 accountHandler = do
