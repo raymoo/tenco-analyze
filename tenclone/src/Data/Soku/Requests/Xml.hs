ba1bfa43bf9e2dee184cb19b28ffecb2dbdce79f
raymoo <notanemail@fakeaddress.net>
2014-09-29 21:45:35 -0700

    outline of xml-parsing modul

---
+++ tenclone/src/Data/Soku/Requests/Xml.hs
@@ -0,0 +1,8 @@
+module Data.Soku.Requests.Xml (
+                                parseNewAccount
+                              , parseResultLog
+                              ) where
+
+import Text.XML.Light
+import Data.Soku.Requests
+
